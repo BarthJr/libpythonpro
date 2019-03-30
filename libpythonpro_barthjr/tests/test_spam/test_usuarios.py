30629a5406383498933640ffd6a441d988436088
BarthJr <juniior.barth@gmail.com>
2019-03-29 21:21:19 -0300

    test(enviador_de_spam): change for produce new tests

--- libpythonpro_barthjr/tests/test_spam/test_usuarios.py
+++ libpythonpro_barthjr/tests/test_spam/test_usuarios.py
@@ -2,13 +2,13 @@ from libpythonpro_barthjr.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Barth')
+    usuario = Usuario(nome='Barth', email='juniior.barth@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
-def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='Barth'), Usuario(nome='Junior')]
+def test_listar_usuarios(sessao):
+    usuarios = [Usuario(nome='Barth', email='barth@gmail.com'), Usuario(nome='Junior', email='juniior@gmail.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
