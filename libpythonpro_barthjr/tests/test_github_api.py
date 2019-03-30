753ac6f241621766ed276b5d01ef20f0847a3db8
BarthJr <juniior.barth@gmail.com>
2019-03-30 20:27:32 -0300

    test(github_api): add pytest-mock

--- libpythonpro_barthjr/tests/test_github_api.py
+++ libpythonpro_barthjr/tests/test_github_api.py
@@ -5,25 +5,24 @@ import pytest
 from libpythonpro_barthjr import github_api
 
 
-def test_buscar_avatar(avatar_url):
-    url = github_api.buscar_avatar('barthjr')
-    assert avatar_url == url
-
-
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars0.githubusercontent.com/u/19367707?v=4'
     resp_mock.json.return_value = {
         'login': 'BarthJr', 'id': 19367707,
         'avatar_url': url, 'gravatar_id': '',
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
+    get_mock = mocker.patch('libpythonpro_barthjr.github_api.requests.get')
+    get_mock.return_value = resp_mock
     yield url
-    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('barthjr')
+    assert avatar_url == url
 
 
 def test_buscar_avatar_integracao():
-    url = github_api.buscar_avatar('barthjr')
-    assert 'https://avatars0.githubusercontent.com/u/19367707?v=4' == url
+    url = github_api.buscar_avatar('barth')
+    assert 'https://avatars2.githubusercontent.com/u/3066749?v=4' == url
